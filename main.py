30b0875dbb080ef318598259adae84e067d48e5c
gebonaut <jiri.geb@gmail.com>
2021-06-05 22:32:22 +0200

    welcome message - done user_database (dictionary) - done user login input (input) - done login program (if statement) - done text options and choice (input) - done input index control (if statement/string and int methods) - done text analyzator program (list comprehension)- done print analysis (string methods) - done gathering data for graph (for loop)- done generating data for graph (while loop/nested for loop/condition) - done
    Complete

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ garpike and stingray are also present.'''
 ]
 
 #general variables
-separator = "_" * 40
+separator = "-" * 40
 
 
 #users database
@@ -63,17 +63,75 @@ else:
     print(separator)
     exit()
 
-#Text options
-text_count = len(TEXTS)
-print(f"We have {text_count} texts to be analyzed.")
+#Text options and choice
+texts_count = len(TEXTS)
+print(f"We have {texts_count} texts to be analyzed.")
 print(separator)
-text_choice = input(f"Enter a number between 1 and {text_count} to select: ")
+input_index = input(f"Enter a number between 1 and {texts_count} to select text: ")
+
+#input index control
+if not input_index.isnumeric():
+    print(separator)
+    print("Input only numeric values! Program now exits...")
+    exit()
+elif int(input_index) not in range(1,texts_count+1):
+    print(separator)
+    print(f"Please insert only number between 1 and {texts_count}! Program now exits...")
+    exit()
+else:
+    text_choice = TEXTS[int(input_index)-1]
+
+#text analyzator
+separated_words = [word.strip(".,!?\n") for word in text_choice.split(" ") if not word == '']
+analyzed_text = {"words" : separated_words,
+                 "words_count" : len(separated_words),
+                 "titlecase" : len([word for word in separated_words if word.istitle()]),
+                 "uppercase" : len([word for word in separated_words if word.isupper() and word.isalpha()]),
+                 "lowercase" : len([word for word in separated_words if word.islower()]),
+                 "numeric_count" : len([word for word in separated_words if word.isnumeric()]),
+                 "numeric_sum" :  sum([int(word) for word in separated_words if word.isnumeric()]),
+                 "words_lenght" : {}
+                 }
+
+#printing out the analysis
+print(separator)
+print(f"""There are {analyzed_text["words_count"]} words in the selected text.
+There are {analyzed_text["titlecase"]} titlecase words.
+There are {analyzed_text["uppercase"]} uppercase words.
+There are {analyzed_text["lowercase"]} lowercase words.
+There are {analyzed_text["numeric_count"]} numeric strings.
+The sum of all the numbers {analyzed_text["numeric_sum"]}""")
+
+#gathering data for graph
+for word in separated_words:
+    if len(word) in analyzed_text["words_lenght"].keys():
+        analyzed_text["words_lenght"][len(word)].append(word)
+    else:
+        analyzed_text["words_lenght"].setdefault(len(word), [word])
+
+#printing out header of graph
+print(separator)
+print("""LEN|     OCCURENCES     |NR.""")
 print(separator)
 
-print(text_choice)
+#printing out data of graph loop
+i = 1
+pokus = 1
+while i <= max(analyzed_text["words_lenght"]): #loop while 'i' is less than longest word
+    for key in analyzed_text["words_lenght"].keys():
+        if key == i:
+            value_count = len(analyzed_text["words_lenght"][key])
+            print(f'{" "*(3-len(str(key)))}{key}|{"*"*value_count}{" "*(20 - value_count)}|{len(analyzed_text["words_lenght"][key])}')
+            pokus = 1
+            i += 1
+        else:
+            if pokus <= max(analyzed_text["words_lenght"]): #in case of non-existing key, repeat till all keys from analyzed_text are looped
+                pokus += 1
+                continue
+            else:
+                pokus = 1
+                i += 1
+                continue
 
 
-
-
-
-
+exit()
